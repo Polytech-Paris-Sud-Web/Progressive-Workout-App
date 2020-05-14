48ab79da5de15c9bf5b4ef483219f9cb051161b4
Thibaud Germain <t.germain2@gmail.com>
2020-05-14 10:48:38 +0200

    redirect https auto

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const compression = require('compression');
+const secure = require('express-force-https');
 const app = express();
 
 const shouldCompress = (req, res) => {
@@ -15,6 +16,7 @@ const shouldCompress = (req, res) => {
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/ProgressiveWorkoutApp'));
+app.use(secure);
 app.use(
   compression({
     // filter decides if the response should be compressed or not,
