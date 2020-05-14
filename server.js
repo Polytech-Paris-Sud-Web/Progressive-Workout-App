327a957c330d462d5416fdf714ff67c751206ac3
Thibaud Germain <t.germain2@gmail.com>
2020-05-14 11:26:40 +0200

    server.js update

--- server.js
+++ server.js
@@ -1,36 +1,14 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+var forceSsl = require('force-ssl-heroku');
 const compression = require('compression');
-const forceSsl = require('force-ssl-heroku');
-const app = express();
 
-const shouldCompress = (req, res) => {
-  if (req.headers['x-no-compression']) {
-    // don't compress responses if this request header is present
-    return false;
-  }
-  // fallback to standard compression
-  return compression.filter(req, res);
-};
+const app = express();
+app.use(compression()).use(forceSsl);
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/ProgressiveWorkoutApp'));
-app.use(forceSsl);
-app.use(
-  compression({
-    // filter decides if the response should be compressed or not,
-    // based on the `shouldCompress` function above
-    filter: shouldCompress,
-    // threshold is the byte threshold for the response body size
-    // before compression is considered, the default is 1kb
-    threshold: 0,
-  })
-);
-
-app.get('*', function (req, res) {
-  res.redirect('https://' + req.headers.host + req.url);
-});
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/ProgressiveWorkoutApp/index.html'));
